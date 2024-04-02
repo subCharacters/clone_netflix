b0266215d950fc0f28f5ca153bd6b190dff8bdb3
subCharacters <wldns59@naver.com>
2024-04-02 21:58:56 +0900

    add class7

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:clone_netflix/screen/home_screen.dart';
+import 'package:clone_netflix/screen/more_screen.dart';
 import 'package:clone_netflix/widget/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -48,11 +49,8 @@ class _MyAppState extends State<MyApp> {
                   child: Text('save'),
                 ),
               ),
-              Container(
-                child: Center(
-                  child: Text('more'),
-                ),
-              )],
+              MoreScreen(),
+            ],
           ),
           bottomNavigationBar: Bottom(),
         ),
